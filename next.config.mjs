a9719d2e094636f1c15fe9fd6d5b44035f59ae6b
Andres Camacho Aguilar <andcamachoa@gmail.com>
2024-08-27 09:13:01 -0600

    new files added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Allow Next.js to handle image optimization for images hosted on openweathermap.org
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'openweathermap.org',
+        },
+      ],
+    },
+  
+    // Set reactStrictMode to false (optional, adjust based on your project's needs)
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
---
+++ src/app/atom.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+import { atom } from "jotai";
+
+export const placeAtom = atom("Republic of Costa Rica");
+
+export const loadingCityAtom = atom(false);
